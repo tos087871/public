b2f1c93c691ea3a05e4ce27c450861bf81ceb52b
tos087871 <a>
2024-02-09 15:22:10 +0900

    update yt.user.js

--- yt.user.js
+++ yt.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Fullscreen 
 // @namespace    http://tampermonkey.net/
-// @version      0.1.11
+// @version      0.1.12
 // @description  try to take over the world!
 // @author       You
 // @match        https://m.youtube.com/*
