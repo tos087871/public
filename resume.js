cbddf48fd3fa3c6b30599c76a33ce51a2c968508
tos087871 <a>
2026-02-10 14:47:43 +0900

    update resume.js

--- resume.js
+++ resume.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Resum
 // @namespace    http://tampermonkey.net/
-// @version      0.1.18
+// @version      0.1.20
 // @description  try to take over the world!
 // @author       You
 // @match        https://m.youtube.com/watch?v=*
