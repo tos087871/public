426b3fb32104cfd15a23a2177050a2e80563e795
tos087871 <a>
2026-02-11 15:53:20 +0900

    update resume.js

--- resume.js
+++ resume.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Resum
 // @namespace    http://tampermonkey.net/
-// @version      0.1.22
+// @version      0.1.23
 // @description  try to take over the world!
 // @author       You
 // @match        https://m.youtube.com/watch?v=*
@@ -20,75 +20,92 @@
   `
 <div class="bpx-player-video-wrap"><video crossorigin="anonymous" preload="auto" src="blob:https://www.bilibili.com/e6ddf136-6439-447b-99ef-16871b83a22e"></video></div>
 `
-if (location.hostname==='8tsu.net') {
-  waitForKeyElements("iframe, frame", function(frame) {
-  frame.addEventListener('load', function(e) {
-    // give main() the `document` from the frame each time it loads
-    if (!e.event.contentDocument.querySelector('video')) {
-      const id = setInterval(()=>{
-        if (e.event.contentDocument.querySelector('video')) {
-          clearInterval(id);
-          main(e.event.contentDocument)
+  if (location.hostname === '8tsu.net') {
+    waitForKeyElements("iframe, frame", function(frame) {
+      frame.addEventListener('load', function(e) {
+        // give main() the `document` from the frame each time it loads
+        if (!e.event.contentDocument.querySelector('video')) {
+          console.log('video not exist')
+          const id = setInterval(() => {
+            if (e.event.contentDocument.querySelector('video')) {
+              clearInterval(id);
+              main(e.event.contentDocument)
+            }
+          }, 1000);
         }
-      }
-      , 1000)
-    }
-    else main(e.event.contentDocument);
-  });
-});
-} else {
-  main(document);
-}
-function main(document) {
-  
-
-  console.log('start')
-  /*
-  const mode = 'test';
-  /*/
-  const mode = 'release';
- // */
-  function getSelector() {
-    //url.hostname
-    if (mode==='test') {
-      return'.html5-main-video';
-    }
-    switch (location.hostname) {
-      case 'm.youtube.com':
-      case 'www.youtube.com':
-        return '.html5-main-video';
-      case 'www.bilibili.com':
-        return '.bpx-player-video-wrap video';
-      case 'ok.ru':
-      case '8tsu.net':
-        return 'video';
-      default:
-        return '';
-    }
+        else main(e.event.contentDocument);
+      });
+    });
+  } else {
+    main(document);
   }
   
-  //window.addEventListener('load',
-  const video = document.querySelector(getSelector());
-  if (!video) {
-    console.log('video not exist')
-    return;
-  }
-  console.log('video exists')
-  const key = 'video resume'+location.url;
-  
-  const preTime = localStorage.getItem(key);
-  if (preTime) video.currentTime = preTime;
-  
-  setInterval(() => {
-    /*if (video.currentTime===video.duration) {
-      
-    }*/
-    localStorage.setItem(key, video.currentTime); //sec
-    console.log(video.currentTime)
-  }, 5 * 1000);
-  
-  function waitVideoElm() {
+  function main(document) {
     
+    
+    console.log('start')
+    /*
+    const mode = 'test';
+    /*/
+    const mode = 'release';
+    // */
+    function getSelector() {
+      //url.hostname
+      if (mode === 'test') {
+        return '.html5-main-video';
+      }
+      switch (location.hostname) {
+        case 'm.youtube.com':
+        case 'www.youtube.com':
+          return '.html5-main-video';
+        case 'www.bilibili.com':
+          return '.bpx-player-video-wrap video';
+        case 'ok.ru':
+        case '8tsu.net':
+          return 'video';
+        default:
+          return '';
+      }
+    }
+    
+    //window.addEventListener('load',
+    
+    
+    
+    const video = document.querySelector(getSelector());
+    if (!video) {
+      console.log('video not exist')
+      const id =setInterval(()=>{
+     const video = document.querySelector(getSelector());
+       if (video) {
+         clearInterval(id)
+         main(video)
+       }
+      },1000);
+      return;
+    }
+    
+    else start(video);
+    
+    function start(video) {
+      
+      console.log('video exists')
+      const key = 'video resume' + location.url;
+      
+      const preTime = localStorage.getItem(key);
+      if (preTime) video.currentTime = preTime;
+      
+      setInterval(() => {
+        /*if (video.currentTime===video.duration) {
+          
+        }*/
+        localStorage.setItem(key, video.currentTime); //sec
+        console.log(video.currentTime)
+      }, 5 * 1000);
+    }
+    
+    function waitVideoElm() {
+      
+    }
   }
-}
 })();
